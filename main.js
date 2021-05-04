3a982ec356c92c041f08eef47df1a23500dd8109
tohzen <melvin.l.mobley@gmail.com>
2021-05-04 17:52:58 -0400

    finished for now

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const button = document.querySelector(".add-place");
-
+const addList = document.querySelector(".list-places");
+const spot = document.querySelector(".spot")
 
 
 function addPlace(obj) {
